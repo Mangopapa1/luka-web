39e05d951714b7a5ddbb33de1e76bc4151b80890
Mangopapa1 <gustp26@gmail.com>
2023-06-13 22:04:43 +0900

    ✨ Form types

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
+import { useState } from 'react';
 import styled from 'styled-components';
-import Circle from './Circle';
 
 function App() {
+    const [value, setValue] = useState("");
+    const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+        const { currentTarget: { value } } = e;
+        setValue(value);
+    }
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log("hello", value);
+
+    }
     return (
         <div>
-            <Circle bgColor="teal" borderColor='yellow' text='hi' />
-            <Circle bgColor="tomato" />
+            <form onSubmit={onSubmit}>
+                <input type="text"
+                    value={value}
+                    placeholder='username' onChange={onChange} />
+                <button>Log in</button>
+            </form>
         </div>
     )
 }
