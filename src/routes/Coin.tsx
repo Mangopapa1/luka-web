6067bc068ed6982210fe39f8472c26fd988ad0cd
Mangopapa1 <gustp26@gmail.com>
2023-10-05 02:41:22 +0900

    ✨ 데이터 api 통신

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,12 +1,58 @@
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Route, useLocation, useParams } from "react-router-dom";
+import styled from "styled-components";
 
 interface Params {
-    coinId: string;
+  coinId: string;
+}
+interface RouteState {
+  name: string;
 }
 function Coin() {
-    const { coinId } = useParams<Params>();
-    console.log(coinId);
-
-    return <h1>Coin:{coinId}</h1>
+  const { coinId } = useParams<Params>();
+  const [loading, setLoading] = useState(true);
+  const { state } = useLocation<RouteState>();
+  const [info, setInfo] = useState({});
+  const [priceInfo, setPriceInfo] = useState({});
+  console.log(state);
+  useEffect(() => {
+    (async () => {
+      const infoData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      setInfo(infoData);
+      setPriceInfo(priceData);
+    })();
+  }, []);
+  return (
+    <Container>
+      <Header>
+        <Title>{state?.name || "Loading.."}</Title>
+      </Header>
+      {loading ? (
+        <>
+          <div>Loading...</div>
+        </>
+      ) : null}
+    </Container>
+  );
 }
+const Container = styled.div`
+  padding: 0 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
 export default Coin;
